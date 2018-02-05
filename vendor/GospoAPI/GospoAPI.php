3ee6eb8d563064f8ef3cd5d6f1a8776060cd98de
raimundoGarcia <raigarva@gmail.com>
2018-02-05 08:52:54 +0100

    Feat():API parte mantenimiento

---
+++ vendor/GospoAPI/GospoAPI.php
@@ -0,0 +1,215 @@
+<?php
+
+require_once 'GospoSQL.php';
+
+class GospoAPI {
+
+    function deleteCentro() {
+        if (isset($_GET['action']) && isset($_GET['id'])) {
+
+            $db = new GospoDB();
+            $db->borrarCentro($_GET['id']);
+            $this->response(204);
+        }
+    }
+
+    function updateCentro() {
+        if (isset($_GET['action']) && isset($_GET['id'])) {
+
+            $obj = json_decode(file_get_contents('php://input'));
+            $objArr = (array) $obj;
+            if (empty($objArr)) {
+                $this->response(422, "error", "Nothing to add. Check json");
+            } else if (isset($obj->nombre)) {
+                $db = new GospoDB();
+                $db->updateCentro($_GET['id'], $obj->nombre, $obj->telefono, $obj->email, $obj->direccion, $obj->municipio, $obj->provincia, $obj->pais, $obj->coordenada_x, $obj->coordenada_y, $obj->url_img);
+                $this->response(200, "success", "Record updated");
+            } else {
+                $this->response(422, "error", "The property is not defined");
+            }
+        }
+    }
+
+    function saveCentro() {
+
+        $obj = json_decode(file_get_contents('php://input'));
+        $objArr = (array) $obj;
+        if (empty($objArr)) {
+            $this->response(422, "error", "Nothing to add. Check json");
+        } else if (isset($obj->nombre)) {
+            $Centro = new GospoDB();
+            $Centro->insertCentro($obj->nombre, $obj->telefono, $obj->email, $obj->direccion, $obj->municipio, $obj->provincia, $obj->pais, $obj->coordenada_x, $obj->coordenada_y, $obj->url_img);
+            $this->response(200, "success", "new record added");
+        } else {
+            $this->response(422, "error", "The property is not defined");
+        }
+    }
+
+    function response($code = 200, $status = "", $message = "") {
+        
+        http_response_code($code);
+        if (!empty($status) && !empty($message)) {
+            $response = array("status" => $status, "message" => $message);
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        }
+    }
+
+    function getCentros() {
+
+        $db = new GospoDB();
+        if (isset($_GET['id'])) {
+            $response = $db->getCentro($_GET['id']);
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        } else {
+            $response = $db->getCentros();
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        }
+    }
+
+    function getPistas() {
+
+        $db = new GospoDB();
+        if (isset($_GET['id'])) {
+            $response = $db->getPistasCentro($_GET['id']);
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        } else {
+            $response = $db->getAllPistas();
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        }
+    }
+
+    function savePista() {
+
+        $obj = json_decode(file_get_contents('php://input'));
+        $objArr = (array) $obj;
+        if (empty($objArr)) {
+            $this->response(422, "error", "Nothing to add. Check json");
+        } else if (isset($obj->id_deporte)) {
+            $pista = new GospoDB();
+            $pista->insertPista($obj->id_deporte, $obj->id_centro, $obj->numero_pista, $obj->precio_hora, $obj->hora_apertura, $obj->hora_cierre);
+            $this->response(200, "success", "new pista added");
+        } else {
+            $this->response(422, "error", "The property is not defined");
+        }
+    }
+
+    function deletePista() {
+        
+        $obj = json_decode(file_get_contents('php://input'));
+        $objArr = (array) $obj;
+        if (empty($objArr)) {
+            $this->response(422, "error", "Nothing to delete. Check json");
+        } else if (isset($obj->id_deporte)) {
+            $pista = new GospoDB();
+            $pista->borrarPista($obj->id_deporte, $obj->id_centro, $obj->numero_pista);
+            $this->response(200, "success", "pista borrada");
+        } else {
+            $this->response(422, "error", "The property is not defined");
+        }
+    }
+
+    function updatePista() {
+
+        $obj = json_decode(file_get_contents('php://input'));
+        $objArr = (array) $obj;
+        if (empty($objArr)) {
+            $this->response(422, "error", "Nothing to add. Check json");
+        } else if (isset($obj->id_centro)) {
+            $db = new GospoDB();
+            $db->updatePista($obj->id_deporte, $obj->id_centro, $obj->numero_pista, $obj->precio_hora, $obj->hora_apertura, $obj->hora_cierre, $obj->url_img);
+            $this->response(200, "success", "Pista updated");
+        } else {
+            $this->response(422, "error", "The property is not defined");
+        }
+    }
+
+    function saveDeporte() {
+
+        $obj = json_decode(file_get_contents('php://input'));
+        $objArr = (array) $obj;
+        if (empty($objArr)) {
+            $this->response(422, "error", "Nothing to add. Check json");
+        } else if (isset($obj->nombre)) {
+            $deporte = new GospoDB();
+            $deporte->insertDeporte($obj->nombre, $obj->descripcion, $obj->tags, $obj->icono_full, $obj->icono_empty, $obj->imagen, $obj->color);
+            $this->response(200, "success", "new Sport added");
+        } else {
+            $this->response(422, "error", "The property is not defined");
+        }
+    }
+
+    function updateDeporte() {
+
+        $obj = json_decode(file_get_contents('php://input'));
+        $objArr = (array) $obj;
+        if (empty($objArr)) {
+            $this->response(422, "error", "Nothing to add. Check json");
+        } else if (isset($obj->id_deporte)) {
+            $db = new GospoDB();
+            $db->updateDeporte($obj->id_deporte, $obj->descripcion, $obj->tags, $obj->icono_full, $obj->icono_empty, $obj->imagen, $obj->color);
+            $this->response(200, "success", "Deporte updated");
+        } else {
+            $this->response(422, "error", "The property is not defined");
+        }
+    }
+
+    function getDeportes() {
+
+        $db = new GospoDB();
+        $response = $db->getAllDeportes();
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+
+    public function API() {
+        header('Content-Type: application/JSON');
+        $method = $_SERVER['REQUEST_METHOD'];
+        switch ($method) {
+            case 'GET':
+                if ($_GET['action'] == 'centros') {
+                    $this->getCentros();
+                } else if ($_GET['action'] == 'pistas') {
+                    $this->getPistas();
+                } else if ($_GET['action'] == 'deportes') {
+                    $this->getDeportes();
+                } else {
+                    $this->response(400);
+                }
+                break;
+            case 'POST':
+                if ($_GET['action'] == 'centros') {
+                    $this->saveCentro();
+                } else if ($_GET['action'] == 'pistas') {
+                    $this->savePista();
+                } else if ($_GET['action'] == 'deportes') {
+                    $this->saveDeporte();
+                } else {
+                    $this->response(400);
+                }
+                break;
+            case 'PUT':
+                if ($_GET['action'] == 'centros') {
+                    $this->updateCentro();
+                } else if ($_GET['action'] == 'pistas') {
+                    $this->updatePista();
+                } else if ($_GET['action'] == 'deportes') {
+                    $this->updateDeporte();
+                } else {
+                    $this->response(400);
+                }
+                break;
+            case 'DELETE':
+                if ($_GET['action'] == 'centros') {
+                    $this->deleteCentro();
+                } else if ($_GET['action'] == 'pistas') {
+                    $this->deletePista();
+                } else {
+                    $this->response(400);
+                }
+                break;
+            default:
+                $this->response(405);
+                break;
+        }
+    }
+
+}
---
+++ vendor/GospoAPI/GospoSQL.php
@@ -0,0 +1,144 @@
+<?php
+
+class GospoDB {
+
+    protected $mysqli;
+
+    const LOCALHOST = '127.0.0.1';
+    const USER = 'root';
+    const PASSWORD = 'root';
+    const DATABASE = 'gosport';
+
+    public function __construct() {
+        try {
+            $this->mysqli = new mysqli(self::LOCALHOST, self::USER, self::PASSWORD, self::DATABASE);
+        } catch (mysqli_sql_exception $e) {
+
+            http_response_code(500);     //se conecta a la base de datos
+            exit;
+        }
+    }
+
+    public function getCentro($id = 0) {
+        $stmt = $this->mysqli->prepare("SELECT * FROM Centros WHERE id_centro=? ; "); //realiza una consulta pasando un id por medio de un prepare statement
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $centros = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
+        return $centros;
+    }
+
+    public function getcentros() {                  // busca todas las perdsonas de la tabla Centro
+        $result = $this->mysqli->query('SELECT * FROM centros');
+        $centros = $result->fetch_all(MYSQLI_ASSOC);
+        $result->close();
+        return $centros;
+        // return json_encode($centros);
+    }
+
+    public function insertCentro($nombre, $telefono, $email, $direccion, $municipio, $provincia, $pais, $coordenada_X, $coordenada_Y, $url_img) {
+        $stmt = $this->mysqli->prepare("INSERT INTO Centros VALUES (null,?,?,?,?,?,?,?,?,?,?,null); ");   //inserta en la tabla personas una persona de nombre "name"
+        $stmt->bind_param('ssssssssss', $nombre, $telefono, $email, $direccion, $municipio, $provincia, $pais, $coordenada_X, $coordenada_Y, $url_img);
+        $r = $stmt->execute();
+        $stmt->close();
+        return $r;
+    }
+
+    public function borrarCentro($id = 0) {             //borra de la tabla personas el usuario con id = id
+        $stmt = $this->mysqli->prepare("DELETE FROM Centros WHERE id_centro =?;");
+        $stmt->bind_param('i', $id);
+        $r = $stmt->execute();
+        $stmt->close();
+        return $r;
+    }
+
+    public function updateCentro($id, $nombre, $telefono, $email, $direccion, $municipio, $provincia, $pais, $coordenada_X, $coordenada_Y, $url_img) {            // cambia el nombre de un usuario con id X por otro nombre insertado
+        if ($this->checkIDCentro($id)) {
+            $stmt = $this->mysqli->prepare("UPDATE Centros SET nombre=?,telefono=?,email=?,direccion=?,municipio=?,provincia=?,pais=?,coordenada_x=?,coordenada_y=?,url_img =? WHERE id_centro = ? ; ");
+            $stmt->bind_param('sssssssssss', $nombre, $telefono, $email, $direccion, $municipio, $provincia, $pais, $coordenada_X, $coordenada_Y, $url_img, $id);
+            $r = $stmt->execute();
+            $stmt->close();
+            return $r;
+        }
+        return false;
+    }
+
+    public function checkIDCentro($id) {              // busca a alguien con el id insertado, y si existe devuelve true
+        $stmt = $this->mysqli->prepare("SELECT * FROM Centros WHERE id_centro=?");
+        $stmt->bind_param('s', $id);
+        if ($stmt->execute()) {
+            $stmt->store_result();
+            if ($stmt->num_rows == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getPistasCentro($id = 0) {
+        $stmt = $this->mysqli->prepare("select p.*,d.nombre from pistas_deporte_centro p inner join deportes d
+        on p.id_deporte = d.id_deporte
+        where id_centro = ? ; "); //realiza una consulta pasando un id por medio de un prepare statement
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $pistas = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
+        return $pistas;
+    }
+
+    public function getAllPistas() {                  // busca todas las pistas de la tabla pistas_deporte_centro
+        $result = $this->mysqli->query('select p.*,d.nombre from pistas_deporte_centro p inner join deportes d
+        on p.id_deporte = d.id_deporte;');
+        $pistas = $result->fetch_all(MYSQLI_ASSOC);
+        $result->close();
+        return $pistas;
+        
+    }
+
+    public function insertPista($id_deporte, $id_centro, $numero_pista, $precio_hora, $hora_apertura, $hora_cierre) {
+        $stmt = $this->mysqli->prepare("INSERT INTO pistas_deporte_centro VALUES (?,?,?,?,?,?,null,null,null); ");   //inserta en la tabla pistas una nueva pista
+        $stmt->bind_param('iiidss', $id_deporte, $id_centro, $numero_pista, $precio_hora, $hora_apertura, $hora_cierre);
+        $r = $stmt->execute();
+        $stmt->close();
+        return $r;
+    }
+
+    public function borrarPista($id_deporte, $id_centro, $numero_pista) {             //borra de la tabla personas el usuario con id = id
+        $stmt = $this->mysqli->prepare("DELETE FROM pistas_deporte_centro WHERE id_deporte =? and id_centro=? and numero_pista=?;");
+        $stmt->bind_param('iii', $id_deporte, $id_centro, $numero_pista);
+        $r = $stmt->execute();
+        $stmt->close();
+        return $r;
+    }
+
+    public function updatePista($id_deporte, $id_centro, $numero_pista, $precio_hora, $hora_apertura, $hora_cierre, $url_img) {            // cambia datos de una pista
+        $stmt = $this->mysqli->prepare("UPDATE pistas_deporte_centro SET precio_hora=?,hora_apertura=?,hora_cierre=?,url_img=? WHERE id_deporte=? and id_centro = ? and numero_pista=?; ");
+        $stmt->bind_param('dsssiii', $precio_hora, $hora_apertura, $hora_cierre, $url_img, $id_deporte, $id_centro, $numero_pista);
+        $r = $stmt->execute();
+        $stmt->close();
+        return $r;
+    }
+    public function insertDeporte($nombre,$descripcion,$tags,$icono_full,$icono_empty,$imagen,$color) {
+        $stmt = $this->mysqli->prepare("INSERT INTO deportes VALUES (null,?,?,?,?,?,?,?); ");   //inserta en la tabla deportes uno nuevo
+        $stmt->bind_param('sssssss', $nombre,$descripcion,$tags,$icono_full,$icono_empty, $imagen, $color);
+        $r = $stmt->execute();
+        $stmt->close();
+        return $r;
+    }
+ public function updateDeporte($id_deporte, $descripcion, $tags, $icono_full, $icono_empty, $imagen, $color) {            // cambia datos de una pista
+        $stmt = $this->mysqli->prepare("UPDATE deportes SET descripcion=?,tags=?,icono_full=?,icono_empty=?,imagen=?,color=? WHERE id_deporte=?; ");
+        $stmt->bind_param('sssssss', $descripcion, $tags, $icono_full, $icono_empty, $imagen, $color, $id_deporte);
+        $r = $stmt->execute();
+        $stmt->close();
+        return $r;
+    }
+    public function getAllDeportes() {                  // busca todas las pistas de la tabla pistas_deporte_centro
+        $result = $this->mysqli->query('select * from deportes;');
+        $pistas = $result->fetch_all(MYSQLI_ASSOC);
+        $result->close();
+        return $pistas;
+        
+    }
+}
