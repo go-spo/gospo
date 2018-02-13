542b54360687d0b8dde54364c29a390c4aced98e
dani-alba <danialba96@gmail.com>
2018-02-14 00:31:46 +0100

    fix():Despliegue script

--- despliegue/despliegue.sh
+++ despliegue/despliegue.sh
@@ -1,12 +1,8 @@
 mkdir /opt/gospo/www
 mkdir /opt/gospo/database
 mkdir /opt/gospo/mysql
-cp -r /opt/gospo/gospo/* /opt/gospo/www
+cp -r /opt/gospo/gospo/www/* /opt/gospo/www
 cp -r /opt/gospo/gospo/database/* /opt/gospo/database
-sleep 10
-rm -rf /opt/gospo/www/database
-rm -rf /opt/gospo/www/docs
-rm -rf /opt/gospo/www/despliegue
 docker run -d -p 80:8080 --net=red_docker --ip 192.168.1.68 --name gospo2 -v /opt/gospo/www:/var/www/html logongas/apache2-php7-ssl
 docker run -d -p 3306:3306 --net=red_docker --ip 192.168.1.69 --name gospo2-mysql -v /opt/gospo/mysql:/var/lib/mysql -v /opt/gospo/database:/database -e MYSQL_ROOT_PASSWORD=root mysql
 sleep 30 
