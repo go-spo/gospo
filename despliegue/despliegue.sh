ffe7c0f6dcf15657d4e9694d937b857ae73f27fe
root <root@localhost.localdomain>
2018-02-13 13:39:26 +0100

    fix():DataBase import name

--- despliegue/despliegue.sh
+++ despliegue/despliegue.sh
@@ -10,4 +10,4 @@ rm -rf /opt/gospo/www/despliegue
 docker run -d -p 80:8080 --net=red_docker --ip 192.168.1.68 --name gospo2 -v /opt/gospo/www:/var/www/html logongas/apache2-php7-ssl
 docker run -d -p 3306:3306 --net=red_docker --ip 192.168.1.69 --name gospo2-mysql -v /opt/gospo/mysql:/var/lib/mysql -v /opt/gospo/database:/database -e MYSQL_ROOT_PASSWORD=root mysql
 sleep 30 
-docker exec -i -t gospo2-mysql /bin/bash /database/import.sh
+docker exec -i -t gospo2-mysql /bin/bash /database/import3.sh
